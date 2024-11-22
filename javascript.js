756a638ebcbfb800b83e152a3f716cc0ae3a8ee5
Shreetej Hadge <shreetejhadge1308@gmail.com>
2024-11-21 21:49:53 -0700

    made the game a round of 5. also made the code to generate a random number in the range of 1-3. that can later be linked to rock or paper or scissors

--- javascript.js
+++ javascript.js
@@ -1,9 +1,18 @@
+console.log("Hello, World!");
 
-playGame() 
+playGame();
 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; ++i) {
+        console.log(getComputerChoice());
+        getHumanChoice;
+    }
+}
 
 function getComputerChoice () {
-
+    return Math.floor(Math.random() * (3 - 1 + 1) + 1);  //Used this formula to find a random number in the range of 1-3: Math.floor(Math.random() * (max - min + 1) + min); The maximum is inclusive and the minimum is inclusive
 }
 
 function getHumanChoice () {
